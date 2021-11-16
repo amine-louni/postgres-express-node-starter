6c6c7426fa9f3352513a4eddc90d37a064735122
Amine Louni <amine.louni.dev@gmail.com>
2021-11-16 21:25:02 +0100

    add validation code

--- src/helpers/formatValidationErrors.ts
+++ src/helpers/formatValidationErrors.ts
@@ -3,7 +3,7 @@ import { ValidationError } from "class-validator";
 export default (errors: ValidationError[]) => {
     const formattedErrors: { field: string, code: string, description: string }[] = errors.map((oneError) => {
         return {
-            code: 'uknown_error', field: oneError.property, description: Object.values(oneError.constraints!).join('\n')
+            code: Object.keys(oneError.constraints!).join(''), field: oneError.property, description: Object.values(oneError.constraints!).join('\n')
         }
     })
     return formattedErrors;
